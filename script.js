9d5e3908d715d2132370406e8f4010f60afdd303
CameronC <cameroncastor05@gmail.com>
2021-09-22 17:12:58 -0600

    added link to questions

--- script.js
+++ script.js
@@ -4,8 +4,19 @@ let SecondsLeft = 80;
 // timer
 let QuizTime = document.getElementById("Quiz-Time");
 
+// scores
+let QuizScores = document.getElementById("Quiz-Scores")
 
-let 
+let buttons = document.getElementById("buttons");
+
+let scoresBtn = document.getElementById("scores")
+
+let StartButton  = document.getElementById("Start-Button")
+StartButton.addEventListener("click", setTime);
+
+
+
+
 
 
 
