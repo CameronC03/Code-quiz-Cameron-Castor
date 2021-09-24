4693e7576615cebadacffb8c1a7050ec27d560ca
CameronC <cameroncastor05@gmail.com>
2021-09-24 00:01:36 -0600

    added some questions

--- assets/questions.js
+++ assets/questions.js
@@ -1,20 +1,20 @@
 var quizQuestions = [
     {
-        title: ".",
-        multipleChoice: [".", ".", ".", "."],
-        answer: "."
+        title: ".Commonly used data types DO NOT include:",
+        multipleChoice: [".strings", ".booleans", ".alerts", ".numbers"],
+        answer: ".alerts"
     },
 
     {
-        title: ".",
-        multipleChoice: [".", ".", ".", "."],
-        answer: "."
+        title: ".The condistion in an if / else statement is enclosed within __.",
+        multipleChoice: [".quotes", ".curly brackets", ".parenthathese", ".square brackets"],
+        answer: ".parenthathese"
     },
 
     {
-        title: ".",
-        multipleChoice: [".", ".", ".", "."],
-        answer: "."
+        title: ".Arrays in JavaScript can be used to store __.",
+        multipleChoice: [".numbers and strings", ".other arrays", ".booleans", ".all of the above"],
+        answer: ".all of the above"
     },
 
     {
