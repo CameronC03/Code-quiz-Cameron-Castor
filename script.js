65e5195e5dc0c97078140e5c09d00f5c3fbe55f4
CameronC <cameroncastor05@gmail.com>
2021-09-27 14:39:34 -0600

    added some style

--- script.js
+++ script.js
@@ -80,10 +80,97 @@ choices.append(el);
 function getUserScore() {
   timer.remove();
 
-  let 
+  let initInput = document.createElement("input");
+  let afterScoresBtn = document.createElement("input");
+  
+  results.innerHTML = `your score is ${score} enter name`
+  afterScoresBtn.setAttribute("type", "button");
+  afterScoresBtn.setAttribute("value", "Post your score");
+  afterScoresBtn.setAttribute("type", "text");
+  afterScoresBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    let scoresA = definescoresA(storedArray, emptyArray)
+    
+    let init = initInput.value;
+    let scoresAndUsers = {
+      score: score,
+      init: init,
+  };
+ 
+  scoresA.push(scoresAndUsers);
+  saveScore(scoresA);
+  displayAllScore();
+  clearScoreBtn();
+  cancelButton();
+  seeScoresBtn.remove();
+});
+
+results.append(initInput)
+results.append(afterScoresBtn)
 }
 
+const saveScore = (Array) => { 
+  window.localStorage.setItem("highScore", JSON.stringify(Array));
+}
 
+const defineScoresA = (a1, a2) => {
+if(a1 !== null) {
+  return a1
+} else {
+return a2
+}
+}
+
+const removeEventListener = (...els) => {
+  for (let el of els) el.remove();
+}
+
+function displayAllScore() {
+  removeEventListener(timer, results, StartButton)\
+  let scoresA = defineScoresA(storedArray, emptyArray)
+
+  scoresA.forEach(obj => {
+    let scoreA = obj.scoreA
+    let init = obj.init
+    let resultsP = document.createElement("p")
+    resultsP.innerText = `${init}: ${scoreA}`;
+    QuizScores.append(resultsP);
+  });
+}
+
+function seeScores() {
+seeScoresBtn.addEventListener("click" function(event) {
+  event.preventDefault();
+  removeEventListener(timer, StartButton)
+  displayAllScore();
+  removeEventListener(seeScoresBtn);
+  clearScoreBtn();
+  cancelButton();
+});
+
+function cancelButton()
+let cancelBtn = document.createElement("input");
+cancelBtn.setAttribute("value", "go back");
+cancelBtn.setAttribute("type", "button");
+cancelBtn.addEventListener("click", function(event) {
+  event.preventDefault();
+  window.location.reload();
+})
+buttons.append(cancelBtn)
+}
+
+function deleteScoresBtn() {
+  let deleteBtn = document.createElement("input");
+  deleteBtn.setAttribute("value", "delete scores")
+  deleteBtn.setAttribute("type", "button")
+  deleteBtn.addEventListener("click", function(event) {
+    event.preventDefault
+    removeEventListener(QuizScores)
+    window.localStorage.removeItem("highscores")
+  })
+  QuizScores.append(deleteBtn)
+  
+  
 
     
     
